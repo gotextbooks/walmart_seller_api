77bf98115c90e7b6ebe8781f02ba214312812686
Pedro Medina <pedromedina190@gmail.com>
2025-07-10 13:04:56 -0400

    receive body instead of injecting values

--- lib/walmart_seller_api/resources/orders.rb
+++ lib/walmart_seller_api/resources/orders.rb
@@ -26,54 +26,13 @@ module WalmartSellerApi
         post(path)
       end
 
-      def cancel_order(order_id, reason)
+      def cancel_order(order_id, body)
         path = "/v3/orders/#{order_id}/cancel"
-        body = {
-          orderLines: [
-            {
-              orderLineNumber: "1",
-              orderLineStatuses: [
-                {
-                  status: "Cancelled",
-                  cancellationReason: reason,
-                  statusQuantity: {
-                    amount: 1,
-                    unit: "EACH"
-                  }
-                }
-              ]
-            }
-          ]
-        }
-
         post(path, body: build_request_body(body))
       end
 
-      def ship_order(order_id, order_lines)
+      def ship_order(order_id, body)
         path = "/v3/orders/#{order_id}/shipping"
-        body = {
-          orderLines: order_lines.map do |line|
-            {
-              orderLineNumber: line[:order_line_number],
-              orderLineStatuses: [
-                {
-                  status: "Shipped",
-                  statusQuantity: {
-                    amount: line[:quantity],
-                    unit: "EACH"
-                  },
-                  trackingInfo: {
-                    shipDateTime: line[:ship_date_time],
-                    carrierName: line[:carrier_name],
-                    methodCode: line[:method_code],
-                    carrierShipmentId: line[:tracking_number]
-                  }
-                }
-              ]
-            }
-          end
-        }
-
         post(path, body: build_request_body(body))
       end
 
