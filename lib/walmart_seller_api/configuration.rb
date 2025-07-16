8db002de7bcf64888e88030c226321721aa418bf
Lucky Alvin Y. Casipe <luckyalvinyc@gmail.com>
2025-07-16 21:31:38 +0800

    Revert "Remove unused configuration, `auth_url`"
    This reverts commit c432dde4afc6801fc0d564423377e3cfe1ccc44c.

--- lib/walmart_seller_api/configuration.rb
+++ lib/walmart_seller_api/configuration.rb
@@ -32,10 +32,21 @@ module WalmartSellerApi
       end
     end
 
+    def auth_url
+      case environment
+      when :production
+        "https://marketplace.walmartapis.com/v3/token"
+      when :sandbox
+        "https://sandbox.walmartapis.com/v3/token"
+      else
+        raise ArgumentError, "Invalid environment: #{environment}. Must be :sandbox or :production"
+      end
+    end
+
     def validate!
       raise ArgumentError, "client_id is required" if client_id.blank?
       raise ArgumentError, "client_secret is required" if client_secret.blank?
       raise ArgumentError, "Invalid environment: #{environment}" unless %i[sandbox production].include?(environment)
     end
   end
-end
+end 
