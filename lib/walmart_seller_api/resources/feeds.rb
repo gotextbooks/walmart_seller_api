653e630351120dd03ebd989c80d9589b376b4537
Bryan Arboleda <80822041+bryanarboledaa@users.noreply.github.com>
2026-08-10 21:37:40 +0800

    move next_cursor under options

--- lib/walmart_seller_api/resources/feeds.rb
+++ lib/walmart_seller_api/resources/feeds.rb
@@ -19,11 +19,11 @@ module WalmartSellerApi
       end
 
       # Get feed status by feedId
-      def get_feed_status(feed_id, next_cursor = nil, options = {})
+      def get_feed_status(feed_id, options = {})
         path = "/v3/feeds/#{feed_id}"
         query = build_query_params(
           limit: options[:limit],
-          nextCursor: next_cursor,
+          nextCursor: options[:next_cursor],
           offset: options[:offset],
           includeDetails: options[:include_details]
         )
